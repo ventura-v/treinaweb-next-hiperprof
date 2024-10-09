678bf561bc1c63f658e2108a749085318c0415de
Vinicius Ventura <vinicius__20@hotmail.com>
2024-10-09 17:27:44 -0300

    creating "PesquisaProfessorPage" page

---
+++ src/pages/pesquisa-professor.tsx
@@ -0,0 +1,25 @@
+import { Icon, TextField } from "@mui/material";
+import PageTitle from "@ui/components/data-display/PageTitle";
+
+
+export default function PesquisaProfessorPage() {
+
+    return (
+        <>
+            <TextField 
+                sx={{ mt: 3, mb: 1}}
+                label={'Encontre um professor'}
+                InputProps={{
+                    startAdornment: <Icon sx={{ mr: 1 }}>search</Icon>
+                }}
+                fullWidth
+                required
+            />
+
+            <PageTitle 
+                title="Professores encontrados"
+                subtitle="Clique sobre um professor para ver os detalhes e poder marcar uma aula com o mesmo"
+            />
+        </>
+    )
+}
